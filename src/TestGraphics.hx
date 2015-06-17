9a3c365b710745645fea778b6a2ca0261760590f
Justin Mills <jlm@justinfront.net>
2015-06-17 16:43:16 +0100

    Added some more drawing tests to the example. It's clear that js is failing on the quadTo badly. Java fails to do fills correctly and some backgrounds need to be modified.

--- src/TestGraphics.hx
+++ src/TestGraphics.hx
@@ -19,12 +19,39 @@ class TestGraphics{
     }
 
     inline private function draw(){
-        // Red Circle with blue border
-        trace( 'draw red circle' );
+
+        trace( 'draw red circle with a blue outline' );
         g.beginFill( 0xff0000, 1 );
-        g.lineStyle( 1, 0x0000ff, 1 );
+        g.lineStyle( 1, 0x0000ff, 2 );
         g.drawCircle( 100, 100, 30 );
         g.endFill();
+
+        trace( 'draw a yellow equilateral triangle with green outline' );
+        g.beginFill( 0xffff00, 1 );
+        g.lineStyle( 1, 0x00ff00, 2 );
+        g.drawEquilaterialTri( 300, 100, 30, -Math.PI/2 );
+        g.endFill();
+
+        trace( 'draw a purple rectangle with orange outline' );
+        g.beginFill( 0x551a8b, 1 );
+        g.lineStyle( 1, 0xffa500, 2 );
+        g.drawRect( 100-30, 300-30, 60, 60 );
+        g.endFill();
+
+        trace( 'heart quadratic curves' );
+        g.beginFill( 0xC1D208, 1 );
+        g.lineStyle( 1, 0x34DDDD, 5 );
+        var cx = 300;
+        var cy = 300;
+        g.moveTo( cx - 27, cy - 20 );
+        g.quadTo( cx - 15, cy - 30, cx, cy - 15 );
+        g.quadTo( cx + 15, cy - 30, cx + 27, cy - 20 );
+        g.quadTo( cx + 34, cy - 5, cx + 20, cy + 6 );
+        g.quadTo( cx + 25, cy, cx, cy + 30 );
+        g.quadTo( cx - 25, cy, cx - 20, cy + 6 );
+        g.quadTo( cx - 34, cy - 5, cx - 28, cy - 20 );
+        g.endFill();
+
     }
 
 }
